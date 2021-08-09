c1a438806c8aa3252ed3d86bd03cc0020154f7ec
Darasimi-okeowo <okeowoadrasimidavid19@gmail.com>
2021-08-09 12:27:24 +0100

    third-commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
+import React from 'react';
+import {RiTimerLine} from 'react-icons/ri'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     Countdown App
-    </div>
+     <section className="timer-container">
+       <section className="timer">
+         <div className="one">
+           <span className="timer-icon"><RiTimerLine/></span>
+           <h2>COUNTDOWN TIMER</h2>
+           <p>your life starts as soon as you see this</p>
+         </div>
+         <div className="two">
+           <section>
+             <p>45</p>
+             <p><small>Days</small></p>
+           </section>
+           <span>:</span>
+           <section>
+             <p>45</p>
+             <p><small>Hours</small></p>
+           </section>
+           <span>:</span>
+           <section>
+             <p>45</p>
+             <p><small>Minutes</small></p>
+           </section>
+           <span>:</span>
+           <section>
+             <p>45</p>
+             <p><small>Seocnds</small></p>
+           </section>
+         </div>
+       </section>
+     </section>
   );
 }
 
